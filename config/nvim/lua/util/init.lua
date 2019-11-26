7edb4dfe3c482099a574f98ea128fb99f664991d
Alvaro Muñoz <alvaro@pwntester.com>
2019-11-26 16:36:34 +0100

    LSP improvements

--- config/nvim/lua/util/init.lua
+++ config/nvim/lua/util/init.lua
@@ -41,4 +41,15 @@ function buffer_find_root_dir(bufnr, is_root_path)
     end
 end
 
-
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
